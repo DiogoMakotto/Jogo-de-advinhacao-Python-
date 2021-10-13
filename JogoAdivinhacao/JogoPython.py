d178116d95f0cc1d055891aebc15b6b508deb516
MakottoSan <diogomakotto@gmail.com>
2021-10-13 18:44:19 -0300

    Adicionando contador de tentativas

--- JogoAdivinhacao/JogoPython.py
+++ JogoAdivinhacao/JogoPython.py
@@ -3,22 +3,28 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 43
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while(rodada <= total_tentativas):
+    print("Tentativa:", rodada, "de", total_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Voce digitou", chute_str)
+    chute = int(chute_str)
 
-print("Voce digitou", chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-chute = int(chute_str)
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    rodada = rodada + 1
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+
 print("Fim do jogo")
